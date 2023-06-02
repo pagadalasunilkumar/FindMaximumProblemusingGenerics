5c347ba7f30732f6ec33e8b54ad3091afe7f13a6
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-06-02 09:23:30 +0530

    [sunil] the file is added

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FindMaximumProblemUsingGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ UC-1MaximumNumbers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FindMaximumProblemUsingGenerics
+{
+    public class MaximumFinder<T> where T : IComparable<T>
+    {
+        public T FindMaximum(T a, T b, T c)
+        {
+            T max = a;
+
+            if (b.CompareTo(max) > 0)
+            {
+                max = b;
+            }
+
+            if (c.CompareTo(max) > 0)
+            {
+                max = c;
+            }
+
+            return max;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MaximumFinder<int> maximumFinder = new MaximumFinder<int>();
+
+            // Test Case 1: Max Number at 1st position
+            int max1 = maximumFinder.FindMaximum(5, 2, 3);
+            Console.WriteLine("Max number: " + max1);
+
+            // Test Case 2: Max Number at 2nd position
+            int max2 = maximumFinder.FindMaximum(1, 4, 2);
+            Console.WriteLine("Max number: " + max2);
+
+            // Test Case 3: Max Number at 3rd position
+            int max3 = maximumFinder.FindMaximum(3, 1, 6);
+            Console.WriteLine("Max number: " + max3);
+        }
+    }
+}
